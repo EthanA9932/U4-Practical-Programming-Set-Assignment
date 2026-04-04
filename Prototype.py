818710a059fe05fc70adbc2c24d02c2cd8ab29fe
Ethan Ayestaran <162427958+ethanayestaran@users.noreply.github.com>
2026-04-04 23:37:14 +0100

    Updated Project
    Updated Prototype.py in order to validate the length of the input 'search_query', changed how the dictionary stores its data.

--- Prototype.py
+++ Prototype.py
@@ -28,68 +28,110 @@
 # | 9. Save results of analysis to an output file.                                    |     N     |
 # +-----------------------------------------------------------------------------------+-----------+
 
-# Creating variables and setting up foundational information --------------------------------------+
-print("Available vendors:\n 1. Dolly Dogs\n 2. Korner Kart\n")                                     #
-available_vendors = ['Dolly Dogs', 'Korner Kart']                                                  #
-metadata = []                                                                                      #
-search_query = str(input("Please enter the name of the vendor you would like to search (2-25): ")) #
-vendor_check = False                                                                               #
-available_check = False                                                                            #
-#--------------------------------------------------------------------------------------------------+
+# Creating variables and setting up foundational information ----------------------------------------+
+print("Available vendors:\n 1. Dolly Dogs\n 2. Korner Kart")                                         #
+available_vendors = ['blank', 'Dolly Dogs', 'Korner Kart']                                           #
+metadata = []                                                                                        #
+search_query = str(input("\nPlease enter the name of the vendor you would like to search (2-25): ")) #
+length_check = False                                                                                 #
+available_check = False                                                                              #
+#----------------------------------------------------------------------------------------------------+
 
 # Looping validations for 'search_query' ---------------------------------------------------+ #[D]
-while vendor_check == False and available_check == False:                                   #
+while length_check != True or available_check != True:                                      #
     # Length Validaton -------------------------------------+                               #
     if len(search_query) >= 2 and len(search_query) <= 25:  #                               #
-        vendor_check = True                                 #                               #
+        length_check = True                                 #                               #
         # Presence Check ----+                              #                               #
     elif search_query == "": #                              #                               #
-        vendor_check = False #                              #                               #
+        length_check = False #                              #                               #
         #--------------------+                              #                               #
     else:                                                   #                               #
-        vendor_check = False                                #                               #
+        length_check = False                                #                               #
     #-------------------------------------------------------+                               #
     # Lookup Validation -------------------------+                                          #
-    for i in [0, len(available_vendors)-1]:      #                                          #
-        if search_query == available_vendors[i]: #                                          #
-            available_check = True               #                                          #
-            break                                #                                          #
-        else:                                    #                                          #
-            available_check = False              #                                          #
+    if search_query in available_vendors:        #                                          #
+        available_check = True                   #                                          #
     #--------------------------------------------#                                          #
-    if vendor_check == False and available_check == False:                                  #
+    if length_check != True or available_check != True:                                     #
         search_query = str(input("Vendor not found, or name out of length range (2-25): ")) #
 #-------------------------------------------------------------------------------------------#
 
-# Dictionary handling ----------------+
-file = open("Hotdogs.txt", "r")       #
-for line in file:  #[A]               #
-    if line.find(search_query) != -1: #
-        data = line.split(",")        #
-        data == {  #[B]               #
-            "vendor_id": data[0],     #
-            "vendor_name": data[1],   #
-            "year_week": data[2],     #
-            "vegan_hotdogs": data[3], #
-            "meat_hotdogs": data[4],  #
-            "onions": data[5],        #
-            "ketchup": data[6]        #
-        }                             #
-    else:                             #
-        continue                      #
-    # Adding dictionary to list --+   #
-    metadata.append(data)         #   #
-    print(metadata)               #   #
-    #-----------------------------+   #
-#-------------------------------------#
+# Dictionary handling ----------------------+ [E]
+file = open("hotdogs.txt", "r")             #
+for line in file:  #[A]                     #
+    if line.find(search_query) != -1:       #
+        parts = line.strip().split(",")     #
+        data = {                            #
+            "vendor_id": parts[0],          #
+            "vendor_name": parts[1],        #
+            "year_week": parts[2],          #
+            "vegan_hotdogs": int(parts[3]), #
+            "meat_hotdogs": int(parts[4]),  #
+            "onions": float(parts[5]),      #
+            "ketchup": float(parts[6]),     #
+        }                                   #
+    else:                                   #
+        continue                            #
+    # Adding dictionary to list --+         #
+    metadata.append(data)         #         #
+    #-----------------------------+         #
+#-------------------------------------------#
+
+# Defining search algorithms ---------------------------------+ [F]
+def linear_search(items, target):                             #
+    index = 0                                                 #
+    found = False                                             #
+                                                              #
+    while found == False and index < len(items):              #
+        if target == items[index]:                            #
+            found = True                                      #
+            print(f"Found {target} at {index+1}.")            #
+        else:                                                 #
+            index += 1                                        #
+                                                              #
+    if found == False:                                        #
+        print(f"{target} not found.")                         #
+                                                              #
+def binary_search(items, target):                             #
+    found = False                                             # 
+    first = 0                                                 #
+    last = len(items) - 1                                     #
+    midpoint = 0                                              #
+    passes = 0                                                #
+                                                              #
+    while first <= last and found == False:                   #
+        midpoint = (first + last) / 2                         #
+        if type(midpoint) == float:                           #
+            midpoint += 0.5                                   #
+            midpoint = int(midpoint)                          #
+        if items[midpoint] == target:                         #
+            found = True                                      #
+            print(f"Found {target} after {passes} pass(es).") #
+        elif items[midpoint] < target:                        #
+            first = midpoint + 1                              #
+        else:                                                 #
+            last = midpoint - 1                               #
+        passes += 1                                           #
+#-------------------------------------------------------------+
+
+# User-inputted category search -----------------------------------------------------|
+print("\nAvailable categories:\n 1. vendor_id\n 2. vendor_name\n 3. year_week\n 4. vegan_hotdogs\n 5. meat_hotdogs\n 6. onions\n 7. ketchup")
+user_search = str(input("\nWhat category would you like to search the data in? "))   #
+while user_search not in data:                                                       #
+    user_search = str(input("Category not found, ensure underscores are present: ")) #
+target_filter = user_search                                                          #
+
+
+#------------------------------------------------------------------------------------|
 
 # +------------------+
 # | Repository Links |
 # +------------------+
-# | Line 65 - [A]    |
-# | Line 68 - [B]    |
-# | Line ?? - [?]    |
-# | Line ?? - [?]    |
+# | Line 40 - [D]    |
+# | Line 60 - [E]    |
+# | Line 62 - [A]    |
+# | Line 82 - [F]    |
 # | Line ?? - [?]    |
 # | Line ?? - [?]    |
 # | Line ?? - [?]    |
